adbdc335f533a3f5d44b78d53f187dbee2dd5d9b
Uzoma S. Uzoma <usumg1165@gmail.com>
2022-08-05 20:48:15 +0100

    This commit will: - Add 2 x 8 px padding to my input field and set its maxlength to 15 - Add 8px letter-spacing to my input and turn autocomplete off - Add 'clear-all' and 'clear-one-char' classes to my 'AC' and 'C' keys respectively - Make my calculator to be able to add, subtract, divide and multiply two operands - Create a global variable, 'calcDisplay' to hold the operations to be calculated - Make my calculator to be able to display the results on the display - Make my calculator to be able to "backspace" with the 'C' key and clear the display with the 'AC' key

--- script.js
+++ script.js
@@ -1,1 +1,73 @@
 console.log('UZOMIAN!!!');
+
+/////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////   FUNCTIONS   /////////////////////////////////////////////
+
+///////////////// Calculator Functions //////////////////////////////////
+function add (u, v) {return u + v}
+function subtract (u, v) {return u - v}
+function multiply (u, v) {return u * v}
+function divide (u, v) {return u / v}
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+': return add(num1, num2);
+        case '-': return subtract(num1, num2);
+        case 'x':
+        case 'X': return multiply(num1, num2);
+        case '/':
+        case '\u00f7': return divide(num1, num2);
+    }
+}
+
+///////////////////// Event Handlers ////////////////////////////////////
+function displayContent(e) {
+    const number = e.target.classList.length ? e.target.querySelector('span') : e.target;
+
+    if (/[ca=]/i.test(number.textContent)) return;
+    calcDisplay += number.textContent;
+    display.value = calcDisplay;
+}
+
+function displayResult() {
+    const formula = showFormula(calcDisplay);
+    display.value = calcDisplay = operate(formula.operator, +formula.num[0], +formula.num[1]);
+}
+
+function clearOneChar() {
+    display.value = calcDisplay = String(calcDisplay).slice(0, -1);
+}
+
+function clearDisplay() {
+    display.value = calcDisplay = '';
+}
+
+///////////////////// Helpers ////////////////////////////////////////////
+function showFormula (str) {
+    return {
+        num: str.split(/[^0-9.]/i), 
+        operator: str.replace(/[0-9.]/g, ''),
+    };
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////
+///////////////   VARIABLES   ///////////////////////////////////////////////////////////
+
+// DOM
+const display = document.querySelector('input[type="text"]');
+const keys = document.querySelector('.keys');
+const answer = document.querySelector('.answer');
+const clearOne = document.querySelector('.clear-one-char');
+const clearAll = document.querySelector('.clear-all');
+
+// Other
+let calcDisplay = '';
+
+// Event Listeners
+keys.addEventListener('click', displayContent);
+answer.addEventListener('click', displayResult);
+clearOne.addEventListener('click', clearOneChar);
+clearAll.addEventListener('click', clearDisplay);
